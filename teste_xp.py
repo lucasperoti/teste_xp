2ba09bee734b0c7e49f166d39b0963e292cbbae0
Lucas Peroti <lucas.dias@primecontrol.com.br>
2022-07-07 21:13:28 -0300

    Adcionando as funçoes para obter a data com mais lançamentos e o launch site com mais lançamentos

--- teste_xp.py
+++ teste_xp.py
@@ -13,9 +13,10 @@ class Teste_xp():
         """
             Função principal para chamar e executar as funçoes da classe.
         """
-        retorno = self.get_api_response(self.url)
-        print(retorno)
-
+        json_response = self.get_api_response(self.url)
+        most_lauch_year = self.date_with_most_launchs(json_response)
+        most_lauch_site = self.launch_site_with_most_launchs(json_response)
+        
     def get_api_response(self,api_url):
         """
             Função que realiza o Get no endpoint de uma api e retorna o resultado convertido em json.
@@ -32,6 +33,40 @@ class Teste_xp():
             return json_response
         except Exception as e:
             logging.error(f"erro durante o processamento da api: {e}")
+
+    def date_with_most_launchs(self,api_json):
+        """
+            Funçao que recebe o json da api da SpaceX e retorna o ano com mais lançamentos de foguetes
+            :param api_json: recebe o json da api 
+            :type api_json: json
+            :return: retorna o ano com mais lançamentos
+            :type return: str
+        """
+        try:
+            list_years = [k['launch_year'] for k in api_json if k.get('launch_year')]
+            most_launch_year = max(set(list_years), key = list_years.count)
+            logging.info(f"ano com mais lançamentos: {most_launch_year}")
+            return most_launch_year
+        except Exception as e:
+            logging.error(f"erro ao separar a data com mais lançamentos: {e}")
+
+    def launch_site_with_most_launchs(self,api_json):
+        """
+            Funçao que recebe o json da api da SpaceX e retorna o launch site com mais lançamentos de foguetes
+            :param api_json: recebe o json da api 
+            :type api_json: json
+            :return: retorna o launch site com mais lançamentos
+            :type return: str
+        """
+        try:
+            list_sites = [k['launch_site'] for k in api_json if k.get('launch_site')]
+            site_names = [k['site_name_long'] for k in list_sites if k.get('site_name_long')]
+            most_site_name = max(set(site_names), key = site_names.count)
+            logging.info(f"launch_site com mais lançamentos: {most_site_name}")
+            return most_site_name
+        except Exception as e:
+            logging.error(f"erro ao separar o launch site com mais lançamentos: {e}")
+
 
 if __name__ == '__main__':
     teste = Teste_xp()
